32950df61a2f8fb695d75eef73ad12b692969dc8
veryspry <ehlinger.matt@gmail.com>
2018-06-15 11:06:50 -0500

    test commit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const app = express();
 
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/articles", require("./routes/articles"));
+
+app.get("/", (req, res) => {
+  res.redirect("/articles");
+})
+
+const PORT = 1337;
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
